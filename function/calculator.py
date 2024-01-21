987aec8f47a447e1426cf0474760384951c09c18
moshiurrahman101 <99rmhridoy@gmail.com>
2024-01-21 15:01:23 +0600

    Chapter-1

---
+++ function/calculator.py
@@ -0,0 +1,20 @@
+# import our basic math function from basicMath file.
+from basicMath import * 
+
+# Call add() function & print
+print("33 + 40 = ", add(33, 40))
+
+# Call sub() function & print
+print("50 - 10 = ", sub(50, 10))
+
+# Call mul() function & print
+print("50 * 13 = ", mul(50, 13))
+
+# Call div() function & print
+print("75 / 15 = ", div(75, 15))
+
+# Call expo() function & print
+print("4 ^ 2 = ", expo(4, 2))
+
+# Call expo() function & print
+print("14 // 3 = ", floorDiv(14, 3))
